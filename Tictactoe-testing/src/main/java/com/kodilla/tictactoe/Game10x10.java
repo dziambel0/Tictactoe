5024e4f9d9b8a5e6f9f26aa43b752531801ed13f
Kamil Wrobel <wrobelkam@gmail.com>
2023-09-26 21:06:18 +0200

    Wersja 1.4

--- Tictactoe-testing/src/main/java/com/kodilla/tictactoe/Game10x10.java
+++ Tictactoe-testing/src/main/java/com/kodilla/tictactoe/Game10x10.java
@@ -60,14 +60,13 @@ public class Game10x10 {
 
     public void printBoard(char[][] board, int wymiar){
 
-        //Druk naglówki kolumn
+
         System.out.print("\t");
         for (int i=0; i<wymiar; i++){
             System.out.print(i+"\t");
         }
         System.out.println();
 
-        //Druk zawartości
         for (int wiersz = 0; wiersz<wymiar; wiersz++){
             System.out.print(wiersz + "\t");
             for (int kolumna = 0; kolumna<wymiar; kolumna++){
